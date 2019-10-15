1c858d7ce3b43d2e529b38c58e1c355b2522071b
jimgbae <bjmg1325@gmail.com>
2019-10-15 20:53:57 +0900

    1015

---
+++ Unity/Human/Assets/Script/Singleton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T instance = null;
+    
+    public static T GetInstance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType(typeof(T)) as T;
+                if (instance == null)
+                    Debug.LogError("No Instance Object");
+            }
+
+            return instance;
+        }
+    }
+
+}
+
+
