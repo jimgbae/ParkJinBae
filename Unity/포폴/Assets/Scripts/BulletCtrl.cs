dcff3e00082a04a55ee87cc7554ecca8266231cf
jimgbae <bjmg1325@gmail.com>
2019-10-30 20:58:04 +0900

    1030

---
+++ Unity/포폴/Assets/Scripts/BulletCtrl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletCtrl : MonoBehaviour
+{
+    //총알 데미지 및 속도
+    public float damage = 20.0f;
+    public float speed = 1000.0f;
+
+    void Start()
+    {
+        GetComponent<Rigidbody>().AddForce(transform.forward * speed);
+    }
+    
+    void Update()
+    {
+        
+    }
+}
